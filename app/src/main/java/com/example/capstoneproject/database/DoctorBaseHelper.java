c9e6a8548e395af5e740a79f1ed1d97ee84fbde5
Enes Karaaslan <fromsmyrna237@gmail.com>
2023-02-24 03:27:09 -0500

    part of basehelper

---
+++ app/src/main/java/com/example/capstoneproject/database/DoctorBaseHelper.java
@@ -0,0 +1,64 @@
+package com.example.capstoneproject.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.capstoneproject.DoctorDetails;
+import com.example.capstoneproject.database.DoctorDbSchema.DoctorTable;
+
+import java.util.ArrayList;
+
+public class DoctorBaseHelper extends SQLiteOpenHelper {
+    public static final int VERSION=1;
+    public static final String DATABASE_NAME="doctor.db";
+    public DoctorBaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + DoctorTable.NAME + "(" +
+                DoctorTable.Cols.DOCTOR_NAME + " TEXT , " +
+                DoctorTable.Cols.DOCTOR_SPECIALITY+ " TEXT, " +
+                DoctorTable.Cols.DOCTOR_EXPERIENCES + " TEXT, " +
+                DoctorTable.Cols.DOCTOR_PHONE + " TEXT, " +
+                DoctorTable.Cols.CONSULTANT_FEE+" TEXT)"
+        );
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+    private static ContentValues getContentValues(DoctorDetails doctorDetails){
+        ContentValues values = new ContentValues();
+//        values.put(BillingTable.Cols.CLIENT_ID, bill.getClient_id());
+        values.put( DoctorTable.Cols.DOCTOR_NAME,doctorDetails.getDoctor_name());
+        values.put(DoctorTable.Cols.DOCTOR_SPECIALITY, doctorDetails.getDoctor_speciality());
+        values.put(DoctorTable.Cols.DOCTOR_EXPERIENCES, doctorDetails.getDoctor_experiences());
+        values.put(DoctorTable.Cols.DOCTOR_PHONE, doctorDetails.getDoctor_phone());
+        values.put(DoctorTable.Cols.CONSULTANT_FEE, doctorDetails.getConsultant_fee());
+        return values;
+    }
+
+    public boolean addDoctor(DoctorDetails doctorDetails){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = getContentValues(doctorDetails);
+        long insert = db.insert(DoctorTable.NAME, null, values);
+        db.close();
+        if (insert == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+}
