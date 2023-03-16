431ad75b6cbe9b3fc9dc999bef6befe740e40d18
Enes Karaaslan <fromsmyrna237@gmail.com>
2023-03-16 01:58:59 -0400

    acty classv6

--- app/src/main/java/com/example/capstoneproject/DoctorActivtyFragment.java
+++ app/src/main/java/com/example/capstoneproject/DoctorActivtyFragment.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import com.example.capstoneproject.database.DoctorBaseHelper;
 
 
-public class DoctorActivtyFragment extends Fragment{
+public class DoctorActivtyFragment extends Fragment {
     private Button doctorUpdateButton;
     private Button doctoreSearchButton;
     private Button doctorDeleteButton;
@@ -33,6 +33,8 @@ public class DoctorActivtyFragment extends Fragment{
     private EditText doctorExperienceEditText;
     private EditText doctorPhoneEditText;
     private EditText consultancyFeeEditText;
+
+
     private static final String EXTRA_DOCTOR_NAME = "com.example.projectsecondpart.doctor_name";
     private static final String EXTRA_DOCTOR_SPECIALITY = "com.example.projectsecondpart.doctor_speciality";
     private static final String EXTRA_DOCTOR_EXPERIENCE = "com.example.projectsecondpart.doctor_experiences";
@@ -45,33 +47,61 @@ public class DoctorActivtyFragment extends Fragment{
 
     DoctorBaseHelper db;
 
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         db = new DoctorBaseHelper(getActivity());
         fillBillingsArrayList();
 
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View v = inflater.inflate(R.layout.fragment_doctoractivty, container, false);
 
-            doctorNameEditText = (EditText) v.findViewById(R.id.DoctorInfoUDNameeditText);
-            doctorNameEditText.setText(allDoctorArrayList.get(currentIndex).getDoctor_name());
-
-            doctorSpecialityEditText = (EditText) v.findViewById(R.id.DoctorInfoUDSpecialityeditText);
-            doctorSpecialityEditText.setText(allDoctorArrayList.get(currentIndex).getDoctor_speciality());
-
-            doctorExperienceEditText = (EditText) v.findViewById(R.id.DoctorInfoUDExperinceeditText);
-            doctorExperienceEditText.setText(allDoctorArrayList.get(currentIndex).getDoctor_experiences());
-
-            doctorPhoneEditText = (EditText) v.findViewById(R.id.DocotorInfoUDPhoneeditText);
-            doctorPhoneEditText.setText(allDoctorArrayList.get(currentIndex).getDoctor_phone());
-
-            consultancyFeeEditText = (EditText) v.findViewById(R.id.DoctorInfoUDFeeeditText);
-            consultancyFeeEditText.setText(allDoctorArrayList.get(currentIndex).getConsultant_fee());
-            doctorUpdateButton=(Button) v.findViewById(R.id.Updatebutton);
     }
-        protected void fillBillingsArrayList() {
-            allDoctorArrayList = db.allDoctorRecords();
-        }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_doctoractivty, container, false);
+
+        doctorNameEditText = (EditText) v.findViewById(R.id.DoctorInfoUDNameeditText);
+        doctorNameEditText.setText(allDoctorArrayList.get(currentIndex).getDoctor_name());
+
+        doctorSpecialityEditText = (EditText) v.findViewById(R.id.DoctorInfoUDSpecialityeditText);
+        doctorSpecialityEditText.setText(allDoctorArrayList.get(currentIndex).getDoctor_speciality());
+
+        doctorExperienceEditText = (EditText) v.findViewById(R.id.DoctorInfoUDExperinceeditText);
+        doctorExperienceEditText.setText(allDoctorArrayList.get(currentIndex).getDoctor_experiences());
+
+        doctorPhoneEditText = (EditText) v.findViewById(R.id.DocotorInfoUDPhoneeditText);
+        doctorPhoneEditText.setText(allDoctorArrayList.get(currentIndex).getDoctor_phone());
+
+        consultancyFeeEditText = (EditText) v.findViewById(R.id.DoctorInfoUDFeeeditText);
+        consultancyFeeEditText.setText(allDoctorArrayList.get(currentIndex).getConsultant_fee());
+        doctorUpdateButton=(Button) v.findViewById(R.id.Updatebutton);
+        doctorUpdateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DoctorDetails updatedDoctor = new DoctorDetails(doctorNameEditText.getText().toString(), doctorSpecialityEditText.getText().toString(),
+                        doctorExperienceEditText.getText().toString(),
+                        doctorPhoneEditText.getText().toString(),consultancyFeeEditText.getText().toString());
+
+                boolean success = db.upDateDoctor(updatedDoctor);
+                if (success) {
+                    Toast.makeText(getActivity(), "Update Success: "+success, Toast.LENGTH_SHORT).show();
+                    //TODO: go back to mainActivity
+                } else {
+                    Toast.makeText(getActivity(), "Update no Success: "+success, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+        return v;
+
+
+    }
+
+    protected void fillBillingsArrayList() {
+        allDoctorArrayList = db.allDoctorRecords();
+    }
+
 }
