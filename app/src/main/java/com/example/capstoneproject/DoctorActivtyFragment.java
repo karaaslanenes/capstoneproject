e3bdbad431ed2ad8fd8b9f3dc2ca44b7d698e176
Enes Karaaslan <fromsmyrna237@gmail.com>
2023-03-16 00:41:21 -0400

    acty classv1

---
+++ app/src/main/java/com/example/capstoneproject/DoctorActivtyFragment.java
@@ -0,0 +1,18 @@
+package com.example.capstoneproject;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+public class DoctorActivtyFragment {
+    private Button doctorUpdateButton;
+    private Button doctoreSearchButton;
+    private Button doctorDeleteButton;
+    private Button doctorViewButton;
+
+    private EditText doctorNameEditText;
+    private EditText doctorSpecialityEditText;
+    private EditText doctorExperienceEditText;
+    private EditText doctorPhoneEditText;
+    private EditText consultancyFeeEditText;
+
+}
