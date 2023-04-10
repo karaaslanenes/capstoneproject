80bb046c02f0e6bf038c7bfb3db65dabc60df2a7
Enes Karaaslan <fromsmyrna237@gmail.com>
2023-04-10 02:52:26 -0400

    app act v1

---
+++ app/src/main/java/com/example/capstoneproject/AppointmnetActivity.java
@@ -0,0 +1,24 @@
+package com.example.capstoneproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class AppointmnetActivity extends AppCompatActivity {
+    Fragment fragment;
+    FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointmnet);
+
+        fragmentManager = getSupportFragmentManager();
+        fragment = fragmentManager.findFragmentById(R.id.appointmentactivity_fragment);
+
+    }
+
+
+}
