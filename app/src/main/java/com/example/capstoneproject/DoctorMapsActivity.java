5650ddc68e6f93516b07cc685fe15b2a2d80af97
Enes Karaaslan <fromsmyrna237@gmail.com>
2023-04-19 23:14:56 -0400

    last

--- app/src/main/java/com/example/capstoneproject/DoctorMapsActivity.java
+++ app/src/main/java/com/example/capstoneproject/DoctorMapsActivity.java
@@ -1,16 +1,16 @@
 package com.example.capstoneproject;
 
-import android.os.Bundle;
-
 import androidx.fragment.app.FragmentActivity;
 
-import com.example.capstoneproject.databinding.ActivityDoctorMapsBinding;
+import android.os.Bundle;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.capstoneproject.databinding.ActivityDoctorMapsBinding;
 
 public class DoctorMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
